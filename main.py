a9d2d7ae444905ce249aed2bd46b949179abe10a
Gabriel Piercecchi <S1097612@studenti.univpm.it>
2022-03-17 16:56:27 +0100

    Initial commit Inizio progetto 17/03/2022

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Maiusc+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+x = "x"
+y = "y"
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}' + " " + x + " " + y)  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('ciao')
+    print(*range(9))
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
