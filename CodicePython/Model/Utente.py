70d17e6505948fc49bc99dddf6003dac8c33ebc8
Omar Naja <s1097765@studenti.univpm.it>
2022-08-17 19:38:13 +0200

    costruttore Utente

---
+++ CodicePython/Model/Utente.py
@@ -0,0 +1,9 @@
+class Utente:
+    def __init__(self, cellulare, codiceFiscale, cognome, dataNascita, email, nome, password):
+        self.cellulare = cellulare
+        self.codiceFiscale = codiceFiscale
+        self.cognome = cognome
+        self.dataNascita= dataNascita
+        self.email = email
+        self.nome = nome
+        self.password = password
