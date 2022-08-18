5dfb1103e774b18e1e47107a3f0f82274a3d350d
GabrielP <gabriel.piercecchi@gmail.com>
2022-08-18 18:01:57 +0200

    Classe Utente.py aggiornata

--- CodicePython/Model/Utente.py
+++ CodicePython/Model/Utente.py
@@ -1,5 +1,9 @@
+import datetime
+
+
 class Utente:
-    def __init__(self, cellulare, codice_fiscale, cognome, data_nascita, email, nome, password):
+    def __init__(self, cellulare: int, codice_fiscale: "", cognome: "",
+                 data_nascita: datetime.date, email: "", nome: "", password: ""):
         self.cellulare = cellulare
         self.codice_fiscale = codice_fiscale
         self.cognome = cognome
@@ -11,43 +15,43 @@ class Utente:
     def getCellulare(self):
         return self.cellulare
 
-    def setCellulare(self, cellulare):
+    def setCellulare(self, cellulare: int):
         self.cellulare = cellulare
 
     def getCodice_fiscale(self):
         return self.codice_fiscale
 
-    def setCodice_fiscale(self, codice_fiscale):
+    def setCodice_fiscale(self, codice_fiscale: ""):
         self.codice_fiscale = codice_fiscale
 
     def getCognome(self):
         return self.cognome
 
-    def setCognome(self, cognome):
+    def setCognome(self, cognome: ""):
         self.cognome = cognome
 
     def getData_nascita(self):
         return self.data_nascita
 
-    def setData_nascita(self, data_nascita):
+    def setData_nascita(self, data_nascita: datetime.date):
         self.data_nascita = data_nascita
 
     def getEmail(self):
         return self.email
 
-    def setEmail(self, email):
+    def setEmail(self, email: ""):
         self.email = email
 
     def getNome(self):
         return self.nome
 
-    def setNome(self, nome):
+    def setNome(self, nome: ""):
         self.nome = nome
 
     def getPassword(self):
         return self.password
 
-    def setPassword(self, password):
+    def setPassword(self, password: ""):
         self.password = password
 
     # Permette il ritorno in formato String di tutti gli Attributi (getinfoUtente)
@@ -56,7 +60,8 @@ class Utente:
                                self.getCellulare(), self.getData_nascita(), self.getPassword())
 
     # Permette l'aggiornamento di tutti gli attributi dell'oggetto Utente (setinfoUtente)
-    def setinfoUtente(self, nome, cognome, codice_fiscale, email, cellulare, data_nascita, password):
+    def setinfoUtente(self, nome: "", cognome: "", codice_fiscale: "", email: "", cellulare: int,
+                      data_nascita: datetime.datetime, password: ""):
         Utente.setNome(self, nome)
         Utente.setCognome(self, cognome)
         Utente.setCodice_fiscale(self, codice_fiscale)
