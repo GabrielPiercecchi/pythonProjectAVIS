5bdf3bd2bd65036f165df16f19fcc5906ffd230b
Alan Rossi <s1097619@studenti.univpm.it>
2022-08-23 20:51:12 +0200

    Update Donatore.py

--- CodicePython/Model/Donatore.py
+++ CodicePython/Model/Donatore.py
@@ -6,21 +6,30 @@ from CodicePython.Model.Utente import Utente
 
 class Donatore(Utente):
 
-    def __init__(self, cellulare: int, codice_fiscale: "", cognome: "",
-                 data_nascita: datetime.date, email: "", nome: "", password: "", gruppo_sanguigno: "", idoneita: bool ):
-        Utente.__init__(self, cellulare, codice_fiscale, cognome,
-                 data_nascita, email, nome, password)
+    def __init__(self, cellulare: int, codice_fiscale: "", cognome: "", data_nascita: datetime.date,
+                 email: "", nome: "", password: "", gruppo_sanguigno: "", idoneita: bool):
+        Utente.__init__(self, cellulare, codice_fiscale, cognome, data_nascita, email, nome, password)
         self.gruppo_sanguigno = gruppo_sanguigno
         self.idoneita = idoneita
 
-        #### INSERIRE METODI GET E SET
+    def __getGruppo_sanguigno__(self):
+        return self.gruppo_sanguigno
+
+    def __setGruppo_sanguigno__(self, gruppo_sanguigno):
+        self.gruppo_sanguigno = gruppo_sanguigno
+
+    def __getIdoneita__(self):
+        return self.idoneita
+
+    def __setIdoneita__(self, gruppo_sanguigno):
+        self.idoneita = idoneita
 
     def __visualizzaDisponibilita__(self):
         donazioni = []
         disponibili = []
         with open('orari.txt', 'r') as fp:
             for line in fp:  # problema
-                donazione = Donazione(year=, month=, day=, hour=, minute=, disponibile=)  # aggiungere attributo???
+                donazione = Donazione(year, month, day, hour, minute, disponibile)  # aggiungere attributo???
                 donazioni.append(donazione)
             for donazione in donazioni:
                 if donazione.disponibile == True:
@@ -28,4 +37,5 @@ class Donatore(Utente):
                     with open('disponibilita.txt', 'w') as fp:
                         for donazione in donazioni:
                             fp.write(str(donazione.year) + ' ' + str(donazione.month) + ' ' + str(donazione.day) + ' ' +
-                                     str(donazione.hour) + ' ' + str(donazione.minute) + ' ' + str(donazione.disponibile))
+                                     str(donazione.hour) + ' ' + str(donazione.minute) + ' ' + str(
+                                donazione.disponibile))
