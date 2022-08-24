f4278e4bb904a39f0143c343b62aabeef6e2cdf6
Alan Rossi <s1097619@studenti.univpm.it>
2022-08-24 15:55:19 +0200

    Update Personale.py

--- CodicePython/Model/Personale.py
+++ CodicePython/Model/Personale.py
@@ -11,7 +11,7 @@ class Personale(Utente):
                  orariolavorativo: datetime.date, stato: bool, turno: datetime.date):
         super().__init__(cellulare, codice_fiscale, cognome, data_nascita, email, nome, password)
         self.idoneita118 = idoneita118
-        self.orariolavorativo = orariolavorativo
+        self.orario_lavorativo = orario_lavorativo
         self.stato = stato
         self.turno = turno
     
@@ -19,7 +19,7 @@ class Personale(Utente):
         info = self.getinfoUtente()
         info += {
             "Idoneità 118": self.idoneita118,
-            "Orario lavorativo": self.orariolavorativo,
+            "Orario lavorativo": self.orario_lavorativo,
             "Stato": self.stato,
             "Turno": self.turno
         }
@@ -28,8 +28,8 @@ class Personale(Utente):
     def getIdoneita118(self):
         return self.idoneita118
 
-    def getOrariolavorativo(self):
-        return self.orariolavorativo
+    def getOrario_lavorativo(self):
+        return self.orario_lavorativo
 
     def getStato(self):
         return self.stato
@@ -39,19 +39,19 @@ class Personale(Utente):
 
     def setinfoPersonale(self, cellulare: int, codice_fiscale: "", cognome: "",
                  data_nascita: datetime.date, email: "", nome: "", password: "", idoneita118: bool,
-                 orariolavorativo: datetime.date, stato: bool, turno: datetime.date):
+                 orario_lavorativo: datetime.date, stato: bool, turno: datetime.date):
         self.setinfoUtente(cellulare, codice_fiscale, cognome,
                  data_nascita, email, nome, password)
         self.idoneita118 = idoneita118
-        self.orariolavorativo = orariolavorativo
+        self.orario_lavorativo = orario_lavorativo
         self.turno = turno
         self.stato = stato
 
     def setIdoneita118(self, idoneita: bool):
         self.idoneita118 = idoneita
 
-    def setOrariolavorativo(self, orario: datetime.date):
-        self.orariolavorativo = orario
+    def setOrario_lavorativo(self, orario: datetime.date):
+        self.orario_lavorativo = orario
 
     def setStato(self, stato: bool):
         self.stato = stato
