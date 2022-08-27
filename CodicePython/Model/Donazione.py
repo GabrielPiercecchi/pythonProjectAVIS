a3ccad459d8df69ffc17ab4125fa5af75249cee5
Alan Rossi <s1097619@studenti.univpm.it>
2022-08-27 14:52:25 +0200

    Update Donazione.py

--- CodicePython/Model/Donazione.py
+++ CodicePython/Model/Donazione.py
@@ -2,11 +2,14 @@ import datetime
 
 from CodicePython.Model.Donatore import Donatore
 
+
 class Donazione:
 
-    def __init__(self, donatore: Donatore, id_donazione: int, year: int, month: int, day: int, hour: int, minute: int, disponibile: bool):
+    def __init__(self, donatore: Donatore, id_donazione: int, tipo: bool, year: int, month: int, day: int, hour: int,
+                 minute: int, disponibile: bool):
         self.donatore = donatore
         self.id_donazione = id_donazione
+        self.tipo = tipo
         self.year = year
         self.month = month
         self.day = day
@@ -26,6 +29,12 @@ class Donazione:
     def __setID_donazione__(self, id_donazione):
         self.id_donazione = id_donazione
 
+    def __getTipo__(self):
+        return self.tipo
+
+    def __setTipo__(self, tipo):
+        self.tipo = tipo
+
     def __getDisponibile__(self):
         return self.disponibile
 
@@ -35,5 +44,5 @@ class Donazione:
     def __getDataPrenotazione__(self):
         return self.data_prenotazione
 
-    def __setDataPrenotazione__(self, data_prenotazione, year: int, month: int, day: int, hour: int, minute: int):
+    def __setDataPrenotazione__(self, year: int, month: int, day: int, hour: int, minute: int):
         self.data_prenotazione = datetime.datetime(year, month, day, hour, minute)
