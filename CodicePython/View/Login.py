755ddacc3c23ab8e9d4910aab69519ac5d64372b
Omar Naja <s1097765@studenti.univpm.it>
2022-08-28 01:11:12 +0200

    Update Login.py

--- CodicePython/View/Login.py
+++ CodicePython/View/Login.py
@@ -1,5 +1,5 @@
 from CodicePython.View.VistaInserisciDonatore import VistaInserisciDonatore
-from PyQt5.QtWidgets import QLabel, QVBoxLayout, QLineEdit, QPushButton, QMessageBox
+from PyQt5.QtWidgets import QLabel, QVBoxLayout, QLineEdit, QPushButton, QMessageBox, QWidget
 
 
 class Login:
@@ -17,7 +17,7 @@ class Login:
         self.qLines["btn_ok"] = btn_ok
         self.v_layout.addWidget(btn_ok)
 
-    def add_info_text(self, param, label): #problema 
+    def add_info_text(self, param, label): #problema
         self.v_layout.addWidget(QLabel(label))
         current_text = QLineEdit(self)
         self.qLines[param] = current_text
