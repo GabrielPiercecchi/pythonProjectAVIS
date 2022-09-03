2f55d80fbf18100bc8b5acf9f7859efd5c1c881c
Omar Naja <s1097765@studenti.univpm.it>
2022-09-03 23:00:57 +0200

    login amministratore

--- CodicePython/View/VistaHome.py
+++ CodicePython/View/VistaHome.py
@@ -1,3 +1,4 @@
+from CodicePython.View.LoginAmministratore import LoginAmministratore
 from CodicePython.View.VistaAmministratore import VistaAmministratore
 from CodicePython.View.VistaDonatori import VistaDonatori
 
@@ -25,8 +26,8 @@ class VistaHome(QWidget):
         return button
 
     def go_amministratore(self):
-        self.vista_amministratore = VistaAmministratore()
-        self.vista_amministratore.show()
+        self.login = LoginAmministratore(callback=LoginAmministratore.log)
+        self.login.show()
 
     def go_donatori(self):
         self.vista_donatori = VistaDonatori()
