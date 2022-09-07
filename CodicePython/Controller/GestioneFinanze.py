aca016da1a047eb23b167ce27b13046b379b289a
GabrielP <gabriel.piercecchi@gmail.com>
2022-09-07 15:09:20 +0200

    Finito VistaDonazioni.py

--- CodicePython/Controller/GestioneFinanze.py
+++ CodicePython/Controller/GestioneFinanze.py
@@ -1,8 +1,15 @@
 import pickle
+from CodicePython import Model
 
 
 class GestioneFinanze:
-    with open('Model/Rapportino.pickle', 'rb') as f:
+    with open('../Model/Rapportino.pickle', 'rb') as f:
         rapportini = list(pickle.load(f))
         for rapportino in rapportini:
             pass
+
+
+if __name__ == '__main__':
+    with open('../Model/Rapportino.pickle', 'rb') as f:
+        rapportini = pickle.load(f)
+        print(rapportini)
