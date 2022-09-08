3e0e81a51f18bfcadf50e316438c1ca1413a8ec5
GabrielP <gabriel.piercecchi@gmail.com>
2022-09-08 16:21:56 +0200

    Codice Finito

--- CodicePython/PyUnitTest/TestGesioneDonatori.py
+++ CodicePython/PyUnitTest/TestGesioneDonatori.py
@@ -14,23 +14,23 @@ class TestGestioneDonatori(TestCase):
         self.amministratore.iscriviDonatore("nome", "cognome", "cf", datetime.datetime(1990, 10, 12), 3333333333,
                                             "email", "password", "AB")
         donatori = None
-        if os.path.isfile('Model/Donatori.pickle'):
-            with open('Model/Donatori.pickle', 'rb') as f:
+        if os.path.isfile('../Model/Donatori.pickle'):
+            with open('../Model/Donatori.pickle', 'rb') as f:
                 donatori = pickle.load(f)
         self.assertIsNotNone(donatori)  #controllo se il file Donatori esiste
         self.assertIn("cf", donatori)  #chiave, elenco
 
     def test_remove_donatore(self):
         donatori = None
-        if os.path.isfile('Model/Donatori.pickle'):
-            with open('Model/Donatori.pickle', 'rb') as f:
+        if os.path.isfile('../Model/Donatori.pickle'):
+            with open('../Model/Donatori.pickle', 'rb') as f:
                 donatori = pickle.load(f)
         self.assertIsNotNone(donatori)
         self.assertIn("cf", donatori)
         self.donatore = self.amministratore.ricercaDonatore("cf")
         self.amministratore.eliminaDonatore("cf")
-        if os.path.isfile('Model/Donatori.pickle'):
-            with open('Model/Donatori.pickle', 'rb') as f:
+        if os.path.isfile('../Model/Donatori.pickle'):
+            with open('../Model/Donatori.pickle', 'rb') as f:
                 donatori = pickle.load(f)
         self.assertIsNotNone(donatori)
         self.assertNotIn("cf", donatori)
